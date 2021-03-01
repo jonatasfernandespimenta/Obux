4f1086bc14fb5c53f419ed3b166fefedaf76ece1
jonatasfernandespimenta <jonatasfernandespimenta@gmail.com>
2021-03-01 14:32:57 -0300

    Changed database location

--- BackEnd/ormconfig.js
+++ BackEnd/ormconfig.js
@@ -1,10 +1,10 @@
 module.exports = {
   type: "mysql",
-  host: "localhost",
+  host: "jhdjjtqo9w5bzq2t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  username: "root",
-  password: "",
-  database: "ObuxNest",
+  username: "kou9og8ejfi0pk0s",
+  password: "hmvvfkod7iw0ir9g",
+  database: "ll1u68lmn6fjf2nf",
   entities: ['dist/**/*.entity.js'],
   synchronize: true
 }
